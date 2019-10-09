2a21217bb8d28220aadc3c795021f0a83b178900
DaviPMello <51029432+DaviPMello@users.noreply.github.com>
2019-10-08 22:16:13 -0300

    beans

---
+++ header/controls.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+#include <SDL.h>
+#include "structs.h"
+
+void eventCheck(SDL_Window* window, bool &endFlag, bool &rain, bool &restartFlag, int &highscore, int carState, Menu &menu, SDL_Point mousePosition, Screen &screen);
+
+void restartVars(bool &restartFlag, int &carState, Road &road, NPCCar (&npcCar)[2], CarPiece &carHood, CarPiece (&pieces)[3], int &score, SDL_Point &lampPosition, Night &night, Rain &rain);
+
+SDL_Point getMouseXY(SDL_Point mousePosition);
+
+void carControl(SDL_Renderer* render, Car &car, SDL_Point mousePosition, Screen screen, Rain &rain);
+
+bool carCollision(SDL_Point mousePosition, Car &car, NPCCar (&npcCar)[2], Screen screen, int &carState, Animation &explosion);
+
+#endif // CONTROLS_H
